5f3a08f445037c36261a3924aa09d20ff68e1adf
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-13 15:43:05 +0900

    CPU test

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,22 @@ class CPU {
     private static final int ADD = 2;
     private static final int SUB = 3;
     private static final int HALT = 4;
+
+    private int[][] program = {
+            { MOV, 0 },     // register = memory[0]
+            { ADD, 1 },     // register = register + memory[1]
+            { MOV, 2 },     // memory[2] = register
+            { HALT, 0 }     // stop program execution
+    };
+
+    public void executeProgram() {
+        int pc = 0;
+
+        while(true) {
+            int opcode = program[pc][0];
+            int operand = program[pc][1];
+        }
+    }
 }
 
 public class Main {

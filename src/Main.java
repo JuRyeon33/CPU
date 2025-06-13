2d9b60aeb119b9bbbcd69013cedf5147b480e24a
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-13 15:49:11 +0900

    CPU test2

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@ class CPU {
     public int register = 0;
     public int[] memory = {5, 10, 0};
 
-    private static final int MOV = 1;
-    private static final int ADD = 2;
-    private static final int SUB = 3;
-    private static final int HALT = 4;
+    private static final int LOAD = 1;
+    private static final int SAVE = 2;
+    private static final int ADD = 3;
+    private static final int SUB = 4;
+    private static final int HALT = 5;
 
     private int[][] program = {
-            { MOV, 0 },     // register = memory[0]
+            { LOAD, 0 },     // register = memory[0]
             { ADD, 1 },     // register = register + memory[1]
-            { MOV, 2 },     // memory[2] = register
+            { SAVE, 2 },     // memory[2] = register
             { HALT, 0 }     // stop program execution
     };
 
@@ -20,6 +21,12 @@ class CPU {
         while(true) {
             int opcode = program[pc][0];
             int operand = program[pc][1];
+
+            switch (opcode) {
+                case LOAD:
+                    register = memory[operand];
+                    System.out.println("MOV : Loading memory[" + operand + " (" + memory[0] + ") into register.");
+            }
         }
     }
 }

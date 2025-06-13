153c68d830a8f55e1f43b6c4518907c10f30a5ec
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-13 15:30:17 +0900

    CPU

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+class CPU {
+    public int register = 0;
+    public int[] memory = {5, 10, 0};
+
+    private static final int MOV = 1;
+    private static final int ADD = 2;
+    private static final int SUB = 3;
+    private static final int HALT = 4;
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
